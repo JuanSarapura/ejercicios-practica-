cec9c884af17357ad73e23f4f898d89ef731bb7a
JuanSarapura <juanzarapura@gmail.com>
2023-07-04 00:54:33 -0300

    commit

--- 05-filter-list/src/list/ListView.jsx
+++ 05-filter-list/src/list/ListView.jsx
@@ -1,10 +1,7 @@
 function ListView({ elements, filterItems }) {
   return (
     <div>
-      <input
-        type="text"
-        onChange={filterItems((e) => filterItems(e.target.value))}
-      />
+      <input type="text" onChange={(e) => filterItems(e.target.value)} />
       <ul>
         {elements.map((name, index) => name && <li key={index}>{name}</li>)}
       </ul>
